5f2fc12b20fa41b42427776b8f378c8f11ce0e18
Harshita-Kanal <harshita.kgv@gmail.com>
2021-01-20 04:07:41 +0530

    Add pagination �

--- client/src/components/Jobs.js
+++ client/src/components/Jobs.js
@@ -1,16 +1,73 @@
 import { Typography } from '@material-ui/core';
 import React from 'react'
 // import Typography from '@material-ui/core/Typography';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Job from './Job';
 
 export default function Jobs({jobs}){
+    const numJobs = jobs.length
+    const numPages = Math.ceil(numJobs / 30) 
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 400,
+          flexGrow: 1,
+        },
+    });
+      
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+ 
+    const jobsOnPage = jobs.slice(activeStep * 30, (activeStep * 30) + 30)
+
+    //no of jops per page = 50
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      };
+    
+      const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+      };
+
     return(
         <div><h1 className = "head">Entry level software jobs</h1>
+        <h4 className = "subhead">Found {numJobs} Jobs</h4>
         {
-           jobs.map(
+           jobsOnPage.map(
                 (job, i) => <Job key = {i} job = {job} />
             )
         }
-        </div>   
+         
+        <div style = {{textAlign: 'center'}}>
+            Page {activeStep + 1} of {numPages}
+        </div>
+        <div class = "step-align">
+         <MobileStepper    
+            variant="progress"
+            steps={Math.ceil(numJobs / 30)}
+            position="static"
+            activeStep={activeStep}
+            className={classes.root}
+            nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+                Next
+                <KeyboardArrowRight />
+            </Button>
+            }
+            backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                <KeyboardArrowLeft />
+                Back
+            </Button>
+            }
+        />
+        </div>
+          
+        </div>  
+         
         )
 }
